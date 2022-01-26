5347e3eeb800f59965ee23fd6cce78decbcfbb1b
Jakub Mikolajczyk <Mikolajczyk.Jakub@outlook.com>
2022-01-26 20:06:27 +0100

    Add view engine and body-parser

--- app/app.js
+++ app/app.js
@@ -1,6 +1,17 @@
-var express = require('express')
-var app = express()
+let express = require('express')
+let app = express()
+let bodyParser = require('body-parser')
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
 app.use(require('./routes/index'))
+
 
 app.listen(3000)
